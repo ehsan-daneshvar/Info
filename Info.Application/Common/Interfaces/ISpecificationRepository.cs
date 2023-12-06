7ef5c85d930d5cc8257aa0d69912f7fb41b2f737
ehsan <ehsan.daneshvar.git@gmail.com>
2023-12-06 21:31:57 +0330

    add interfaces

---
+++ Info.Application/Common/Interfaces/ISpecificationRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Info.Domain.Entities;
+
+namespace Info.Application.Common.Interfaces
+{
+    public interface ISpecificationRepository
+    {
+        void Update(Specification entity);
+    }
+}
---
+++ Info.Application/Common/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Info.Application.Common.Interfaces
+{
+    public interface IUnitOfWork
+    {
+        void Save();
+        ISpecificationRepository Specification { get; }
+
+    }
+}
