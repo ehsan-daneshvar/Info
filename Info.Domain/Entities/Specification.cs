18dce542051ed94211eec844d90fa98e2ff5ab99
ehsan <ehsan.daneshvar.git@gmail.com>
2023-12-06 19:00:39 +0330

    Create Entity folder And Add Specification Entity

---
+++ Info.Domain/Entities/Specification.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Info.Domain.Entities
+{
+    public class Specification
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Family { get; set; }
+        public DateTime Birth { get; set; }
+        public string Address { get; set; }
+
+    }
+}
