cdcf2d727306b63790a4e8c563bcf967c5a304b5
ehsan <ehsan.daneshvar.git@gmail.com>
2023-12-07 11:55:01 +0330

    AddIdentity in pragram.cs

--- Info.Web/Program.cs
+++ Info.Web/Program.cs
@@ -1,6 +1,7 @@
 using Info.Application.Common.Interfaces;
 using Info.Infrastructure.Data;
 using Info.Infrastructure.Repository;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,6 +10,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(option =>
     option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
 
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
